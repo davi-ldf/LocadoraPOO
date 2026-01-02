69ec0ccc299777cc845fefd8900f497e5782cd55
Davi Lisboa <davifranca135@gmail.com>
2026-01-01 22:00:48 -0300

    Update Filme.java

--- src/Filme.java
+++ src/Filme.java
@@ -1,2 +1,48 @@
-public class Filme {
+public class Filme extends Midia
+{
+    private String diretor;
+    private int duracaoMinutos;
+
+    public Filme(String titulo, int ano, String diretor, int duracaoMinutos)
+    {
+        super(titulo, ano);
+        this.diretor = diretor;
+        this.duracaoMinutos = duracaoMinutos;
+    }
+
+    public String getDiretor()
+    {
+        return diretor;
+    }
+    public void setDiretor(String diretor)
+    {
+        this.diretor = diretor;
+    }
+    public int getDuracaoMinutos()
+    {
+        return duracaoMinutos;
+    }
+    public void setDuracaoMinutos(int duracao)
+    {
+        this.duracaoMinutos = duracao;
+    }
+
+    @Override
+    public String getTipo()
+    {
+        return "Filme";
+    }
+
+    @Override
+    public double calcularMulta(int diasAtraso)
+    {
+        // Filme: R$ 2,50 por dia de atraso
+        return diasAtraso * 2.50;
+    }
+
+    @Override
+    public String toString()
+    {
+        return id + " - " + titulo + " (" + ano + ") - Dir: " + diretor + " - " + duracaoMinutos + "min";
+    }
 }
